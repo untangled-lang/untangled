46600cffb57ac54c13fcaf395b9f6a5e6e7a75c3
Duru Ugurlu <duru.ugurlu@tufts.edu>
2023-02-18 12:54:31 -0500

    Some updates

--- ast.ml
+++ ast.ml
@@ -1,49 +1,46 @@
 (*
  * @todo - Think about thread specific types
  *)
+
+type id = string;
+
 type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq |
           And | Or | Mod
 
 type uop = Neg | Not | Plusplus | Minmin
 
-type typ = Int | Bool | Float | Unit | Tuple of typ * typ | Thread | String
+type typ = Unit | Bool | Int | Float | String | Thread | Tuple of typ * typ | Array of typ
 
 type bind = typ * string
 
 type expr =
-Literal of int
-| Fliteral of string
+  IntLit of int
+| FloatLiteral of string
 | BoolLit of bool
 | Id of string
 | Binop of expr * op * expr
 | Unop of uop * expr
-| Assign of string * expr
-| Call of string * expr list
+| Assign of string * expr 
+| Spawn of string
 | Noexpr
 
 type stmt =
-  Block of stmt list
+  Block of stmt list 
 | Expr of expr
-| Return of expr
 | If of expr * stmt * stmt
 | For of expr * expr * expr * stmt
 | While of expr * stmt
-(* TODO: Add message sent / receive statements *)
+(* TODO: | Receive of (typ * bind list * stmt) list *)
+| Send of thread_id * expr
+| Decl of typ * string * expr
+(* Note: “and” can be used for mutually recursive definitions https://stackoverflow.com/a/29471895/4414003 *)
 
-(*
- * Threads definition:
- *    name
- * .  message sent/receive?
- *    body: statement?
- *)
 type thread_decl = {
   tname: string;
   body: stmt list;
-  (* TODO: Keep track of message sent / receive? *)
 }
 
 type program = bind list * thread_decl list
-(* type program = bind *)
 
 let rec string_of_typ t =
   match t with
--- toplevel.ml
+++ toplevel.ml
@@ -1,13 +1,5 @@
 open Ast
 
-let rec eval ast = 
-  match ast with
-  (vdecls, tdecls) -> 
-    List.iter (fun (_, v) -> print_string v) vdecls 
-  (* | _ -> print_string "Something else" *)
-
-
 let () =
   let lex_buf = Lexing.from_channel stdin in
-  (* let ast = Parser.program Scanner.token lex_buf in eval ast *)
   let ast = Parser.program Scanner.token lex_buf in Ast.string_of_program ast
