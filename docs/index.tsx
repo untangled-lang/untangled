9f39c78da05df838a85feda1ba3bbcc7cfb32e3f
Luke Taylor <luke@deentaylor.com>
2023-02-22 04:51:03 -0500

    Easier Code component

--- docs/index.tsx
+++ docs/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './src/main.mdx';
-import Code, { CodeWrapper } from './src/components/Code';
+import Code, { CodeConfig } from './src/components/Code';
 
 const root = document.getElementById('root-container');
 if (!root) throw new Error('React root element not found');
@@ -9,6 +9,8 @@ if (!root) throw new Error('React root element not found');
 // Mount the MDX page
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <Main components={{ code: Code, pre: CodeWrapper }} />
+    <CodeConfig theme="github-dark">
+      <Main components={{ code: Code }} />
+    </CodeConfig>
   </React.StrictMode>
 );
--- docs/src/components/Code.tsx
+++ docs/src/components/Code.tsx
@@ -1,6 +1,5 @@
-import React, { useMemo, useContext, createContext } from 'react';
-import { getHighlighter, HighlighterOptions } from 'shiki';
-import parse from 'html-react-parser';
+import React, { useContext, createContext } from 'react';
+import { FontStyle, getHighlighter, HighlighterOptions } from 'shiki';
 
 
 const UNTANGLED_LANGUAGE_DEFINITION = {
@@ -11,13 +10,16 @@ const UNTANGLED_LANGUAGE_DEFINITION = {
 } as const;
 
 const LOAD_LANGUAGES = ['ocaml', 'python', UNTANGLED_LANGUAGE_DEFINITION] as const;
-const LOAD_THEMES = ['poimandres', 'Overnight-Slumber-italic', 'Overnight-Slumber'] as const;
-
+const LOAD_THEMES = ['poimandres', 'Overnight-Slumber-italic', 'Overnight-Slumber', 'nord', 'github-dark', 'github-dark-dimmed'] as const;
 
 // Union types for languages/themes
 type LanguageName<T extends typeof LOAD_LANGUAGES[number]> = T extends object ? T['id'] : T;
 export type Language = LanguageName<typeof LOAD_LANGUAGES[number]>;
 export type Theme = typeof LOAD_THEMES[number];
+
+const DEFAULT_THEME = 'github-dark' satisfies Theme;
+
+
 
 // Create shiki highlighter instance (requires some messy type gymnastics to remove `readonly`)
 type DeepReadonly<T> = T extends Function ? T : T extends object ? { readonly [K in keyof T]: DeepReadonly<T[K]> } : T;
@@ -25,35 +27,56 @@ const options: HighlighterOptions = { themes: LOAD_THEMES, langs: LOAD_LANGUAGES
 const highlighter = await getHighlighter(options);
 
 
-/** Context for the Code component, which allows us to set the theme */
-export const CodeContext = createContext<{ theme: Theme }>({ theme: 'poimandres' });
-export const CodeConfig = CodeContext.Provider;
-
-
-export type CodeProps = {
-  children: string;
-  className?: `language-${Language}`;
-};
-/** Renders code with syntax highlighting */
-export default function Code({
-  children: code,
-  className = 'language-untangled',
-}: CodeProps) {
-  const language = className.replace(/^language-/, '') as Language;
-  const { theme } = useContext(CodeContext);
-  const html = useMemo(() => highlighter.codeToHtml(code, { lang: language, theme }), [code, language]);
-  return <>{parse(html)}</>;
+// Configuration context for the Code component, which allows us to set the theme
+export const CodeConfigContext = createContext<{ theme: Theme }>({ theme: DEFAULT_THEME });
+type ContextValue<T> = T extends React.Context<infer V> ? V : never;
+export function CodeConfig({ children, ...props }: { children?: React.ReactNode } & ContextValue<typeof CodeConfigContext>) {
+  return <CodeConfigContext.Provider value={props}>{children}</CodeConfigContext.Provider>;
 }
 
 
-/** Renders a `pre` tag unless its only child is a Code component, in which case it renders the
- *  child directly. This is a utility to prevent double-rendering `pre` tags. */
-export function CodeWrapper({ children, ...props }: { children: React.ReactNode } & React.HTMLAttributes<HTMLPreElement>) {
-  if (React.Children.count(children) === 1) {
-    const child = React.Children.only(children);
-    if (child && typeof child === 'object' && 'type' in child && child.type === Code) {
-      return children;
-    }
-  }
-  return <pre {...props}>{children}</pre>;
+/** Renders code with syntax highlighting */
+export default function Code({
+  children,
+  className = 'language-untangled',
+  highlight = true,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+  highlight?: boolean;
+}) {
+  const language = className.replace(/^language-/, '') as Language;
+  const { theme } = useContext(CodeConfigContext);
+  // If highlight is disabled, or if children is *not* a single string, play dead.
+  if (typeof children !== 'string' || !highlight) return <code>{children}</code>;
+  // Highlight string child with shiki
+  const lines = highlighter.codeToThemedTokens(children, language, theme);
+
+  return (
+    <code>
+      {lines.map((tokens, lineIdx) => (
+        <span key={lineIdx}>
+          {/* Tokens in this line */}
+          {
+            tokens.map((token, tokenIdx) => (
+              <span
+                key={tokenIdx}
+                style={{
+                  color: token.color,
+                  ...(token.fontStyle ?? 0) & FontStyle.Italic && { fontStyle: 'italic' },
+                  ...(token.fontStyle ?? 0) & FontStyle.Bold && { fontWeight: 'bold' },
+                  ...(token.fontStyle ?? 0) & FontStyle.Underline && { textDecoration: 'underline' },
+                }}
+                data-token={JSON.stringify(token)}
+              >
+                {token.content}
+              </span>
+            ))
+          }
+          {/* Newline between lines */}
+          {lineIdx < lines.length - 1 ? '\n' : null}
+        </span>
+      ))}
+    </code>
+  );
 }
