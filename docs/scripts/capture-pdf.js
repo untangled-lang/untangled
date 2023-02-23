794089d8a41444adaa5d64d3e1ad37095c611988
Luke Taylor <luke@deentaylor.com>
2023-02-23 09:28:53 -0500

    Add script to make PDF of LRM

---
+++ docs/scripts/capture-pdf.js
@@ -0,0 +1,34 @@
+import puppeteer from 'puppeteer';
+import { preview } from 'vite';
+import fs from 'fs/promises';
+
+// Check the app has been built
+const hasDist = await fs.access('dist').then(() => true).catch(() => false);
+if (!hasDist) {
+  console.error('Error: run `vite build` before capturing a PDF');
+  process.exit(1);
+}
+
+// Serve the built app
+const previewServer = await preview({ preview: { port: 5174 } });
+
+// Load the page
+const browser = await puppeteer.launch({ headless: false });
+const page = await browser.newPage();
+await page.goto('http://localhost:5174');
+await page.waitForSelector('h1');
+
+// Capture PDF
+// @ts-ignore
+await page.evaluate(() => document.documentElement.style.zoom = 0.66);
+const contentHeight = await page.evaluate(() => document.documentElement.scrollHeight);
+await page.pdf({
+  width: '8.5in',
+  height: contentHeight,
+  printBackground: true,
+  path: 'untangled.pdf',
+});
+
+// Clean up
+await browser.close();
+previewServer.httpServer.close();
