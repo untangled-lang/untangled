6a4d3108252c03e6f1fbfd03ea1f0f3f8165ae78
Luke Taylor <luke@deentaylor.com>
2023-03-26 22:08:11 -0400

    successful print hello world
    Co-authored-by: cledi01 <caleb.gorrila@gmail.com>
    Co-authored-by: Nick Doan <nickbar01234@gmail.com>

--- src/codegen.ml
+++ src/codegen.ml
@@ -15,10 +15,18 @@ module StringMap = Map.Make(String)
 
 let translate ((tdecls : sthread_decl list), (fdecls : sfunc_decl list)) =
   let context = L.global_context () in
-  let void_t = L.void_type context in
+  let i32_t      = L.i32_type    context
+  and i8_t       = L.i8_type     context
+  and i1_t       = L.i1_type     context
+  and float_t    = L.double_type context
+  and void_t     = L.void_type context in
   let the_module = L.create_module context "Untangled" in
 
   (* TODO - Add type *)
+  let printf_t : L.lltype =
+    L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
+  let printf_func : L.llvalue =
+     L.declare_function "printf" printf_t the_module in
   let print_t : L.lltype = L.function_type void_t [||] in
   let print_func : L.llvalue = L.declare_function "print" print_t the_module in
 
@@ -27,7 +35,7 @@ let translate ((tdecls : sthread_decl list), (fdecls : sfunc_decl list)) =
     let thread_decl m tdecl =
       let stname = tdecl.stname and
           ttype = L.function_type void_t [||] in
-      StringMap.add stname (L.define_function tdecl.stname ttype the_module, tdecl) m in
+      StringMap.add stname (L.define_function (if tdecl.stname = "Main" then "main" else tdecl.stname) ttype the_module, tdecl) m in
     List.fold_left thread_decl StringMap.empty tdecls in
 
   let build_thread_body tdecl =
@@ -37,16 +45,18 @@ let translate ((tdecls : sthread_decl list), (fdecls : sfunc_decl list)) =
 
     let rec expr (builder: L.llbuilder) ((_, sexpr : sexpr)) =
       match sexpr with
-        | SStringLit s -> L.const_stringz context s
+        (* | SStringLit s -> L.const_stringz context s *)
+        | SStringLit s -> L.build_global_stringptr s "tmp" builder
         | SCall ("print", [sexpr]) ->
-            L.build_call print_func [| string_format_str; (expr builder sexpr) |] "print" builder
+            L.build_call printf_func [| string_format_str; (expr builder sexpr) |] "printf" builder
         | _ -> raise (Failure "Implement other exprs builder")
     and stmt (builder: L.llbuilder) = function
       (* TODO - Update SBlock to account for scoping rules *)
       SBlock sblock -> List.fold_left stmt builder sblock
       | SExpr sexpr -> let _ = expr builder sexpr in builder
       | _ -> builder
-
-    in stmt builder (SBlock tdecl.sbody)
+    in
+    let builder = stmt builder (SBlock tdecl.sbody) in
+    L.build_ret_void builder
 
   in let _ = List.map build_thread_body tdecls in the_module
