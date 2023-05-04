2d7c36d4ae6001afb8789bab9522b8c634ce31d0
Luke Taylor <luke@deentaylor.com>
2023-05-04 17:32:47 -0400

    Create new MDX entry point for final report

--- docs/entry-final-report.tsx
+++ docs/entry-final-report.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Main from './src/main';
+import FinalReport from './src/final-report.mdx';
 import Code, { CodeConfig, CodeWrapper } from './src/components/Code';
 import Link from './src/components/Link';
 
@@ -11,7 +11,7 @@ if (!root) throw new Error('React root element not found');
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <CodeConfig theme="poimandres">
-      <Main components={{ code: Code, pre: CodeWrapper, a: Link }} />
+      <FinalReport components={{ code: Code, pre: CodeWrapper, a: Link }} />
     </CodeConfig>
   </React.StrictMode>
 );
---
+++ docs/entry-lrm.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LRM from './src/lrm.mdx';
+import Code, { CodeConfig, CodeWrapper } from './src/components/Code';
+import Link from './src/components/Link';
+
+const root = document.getElementById('root-container');
+if (!root) throw new Error('React root element not found');
+
+// Mount the MDX page
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <CodeConfig theme="poimandres">
+      <LRM components={{ code: Code, pre: CodeWrapper, a: Link }} />
+    </CodeConfig>
+  </React.StrictMode>
+);
