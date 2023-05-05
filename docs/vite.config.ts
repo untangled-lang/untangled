73bf2e3225ce49038b774ecf09542a310cbf5da6
Luke Taylor <luke@deentaylor.com>
2023-05-05 02:01:48 -0400

    make sure github actions deploys all pages

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,10 +3,21 @@ import mdx from '@mdx-js/rollup';
 import rehypeSlug from 'rehype-slug';
 import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { fileURLToPath } from 'url';
 
 
 export default defineConfig({
-  build: { target: 'esnext' },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL('./index.html', import.meta.url)),
+        lrm: fileURLToPath(new URL('./lrm.html', import.meta.url)),
+        tutorial: fileURLToPath(new URL('./tutorial.html', import.meta.url)),
+        finalReport: fileURLToPath(new URL('./final-report.html', import.meta.url)),
+      },
+    }
+  },
   base: process.env.DOCS_BASE_PATH ?? '/',
   plugins: [
     { enforce: 'pre', ...mdx({ rehypePlugins: [rehypeSlug] }) },
